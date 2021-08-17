d01f06fd81747de715edbac318230f86ee1e0d9c
hanjy-slowturtle <h920915@gmail.com>
2021-08-17 21:55:04 +0900

    ~ #2.1 Installing SocketIO

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,37 +10,42 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+const ioServer = SocketIO(httpServer);
 
-const server = http.createServer(app);
-const wsServer = new WebSocket.Server({ server });
-
-let sockets = [];
-
-wsServer.on("connection", (socket) => {
-    socket["nickname"] = "anonymous";
-    sockets.push(socket);
-    console.log(`Connected to Browser, client count: ${sockets.length}`);
-
-    socket.on("message", (message) => {
-        const { type, payload } = JSON.parse(message);
-        switch(type) {
-            case "message":
-                sockets.forEach((aSocket) => {
-                    if(aSocket !== socket) {
-                        aSocket.send(`${socket.nickname}: ${payload.toString()}`);
-                    }
-                });
-                break;
-            case "nickname":
-                socket["nickname"] = payload;
-                break;
-        }
-    });
-    socket.on("close", () => {
-        sockets = sockets.filter((aSocket) => (aSocket !== socket));
-        console.log(`Disconnected from Browser, client count: ${sockets.length}`);
-    });
+ioServer.on("connection", (socket) => {
+    console.log(socket);
 });
 
-server.listen(3000, handleListen);
+// const wsServer = new WebSocket.Server({ server });
+
+// let sockets = [];
+
+// wsServer.on("connection", (socket) => {
+//     socket["nickname"] = "anonymous";
+//     sockets.push(socket);
+//     console.log(`Connected to Browser, client count: ${sockets.length}`);
+
+//     socket.on("message", (message) => {
+//         const { type, payload } = JSON.parse(message);
+//         switch(type) {
+//             case "message":
+//                 sockets.forEach((aSocket) => {
+//                     if(aSocket !== socket) {
+//                         aSocket.send(`${socket.nickname}: ${payload.toString()}`);
+//                     }
+//                 });
+//                 break;
+//             case "nickname":
+//                 socket["nickname"] = payload;
+//                 break;
+//         }
+//     });
+//     socket.on("close", () => {
+//         sockets = sockets.filter((aSocket) => (aSocket !== socket));
+//         console.log(`Disconnected from Browser, client count: ${sockets.length}`);
+//     });
+// });
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
